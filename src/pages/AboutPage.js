377473075811344f85477070908d9708607a5866
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2025-04-04 17:38:20 -0400

    Create AboutPage.js
    Signed-off-by: RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,23 @@
+// src/pages/AboutPage.js
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="about-container">
+      <h2>About Me</h2>
+      <p>I am Lego builder and future designer.</p>
+      <h3>Skills</h3>
+      <ul>
+        <li>HTML & CSS</li>
+        <li>Photoshop</li>
+        <li>Indesign</li>
+        <li>Javascript</li>
+        <li>PHP & MySQL</li>
+      </ul>
+      <h3>Interests</h3>
+      <p>I enjoy traveling, photography of Lego and Figures, and play Video Games.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
