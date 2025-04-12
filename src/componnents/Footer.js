1800cfbd1236f30c1d8c48a972ed4a4243e56284
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2025-04-12 08:13:39 -0400

    Create Footer.js
    Signed-off-by: RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>

---
+++ src/componnents/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2025 My Portfolio. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
