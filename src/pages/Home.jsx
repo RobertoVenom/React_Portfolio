d873cc727bbd4d486c46cada3c03910c39e4299b
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2026-04-08 15:43:31 -0400

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <section className="home">
+      <h1>Hello, I'm Danny 👋</h1>
+      <p>Aspiring Web Developer and Designer</p>
+      <p>I built project in my college life and did some for my free time.</p>
+    </section>
+  );
+}
+
+export default Home;
