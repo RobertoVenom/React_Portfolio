decfd5b131308f20eb916ad49f5794343c78af08
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2026-04-13 16:04:10 -0400

    Revise greeting and enhance project details
    Updated greeting and expanded project description.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 function Home() {
   return (
     <section className="home">
-      <h1>Hello, I'm Danny 👋</h1>
+      <h1>Hello, I'm Danny</h1>
       <p>Aspiring Web Developer and Designer</p>
-      <p>I built project in my college life and did some for my free time.</p>
+      <p>I built project in my college life and did some for my free time like photographing, or even work on the HTML, JS, CSS and other programs.</p>
     </section>
   );
 }
