b81cb8d5238122d351d38b8763099a059108bd3a
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2026-04-13 16:02:59 -0400

    Add About component with personal information

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>I’m passionate about building modern web apps using React.</p>
+
+      <h3>Skills</h3>
+      <ul>
+        <li>HTML, CSS, JavaScript</li>
+        <li>React</li>
+        <li>Git & GitHub</li>
+      </ul>
+    </section>
+  );
+}
+
+export default About;
